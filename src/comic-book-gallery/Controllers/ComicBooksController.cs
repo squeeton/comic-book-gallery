5699b8ae1f7c3d78eb157a2d4adcea053055ed6a
Matthew Standlee <mwstandlee@gmail.com>
2018-05-25 12:51:17 -0500

    Updated detail action method to use action reult types

--- src/comic-book-gallery/Controllers/ComicBooksController.cs
+++ src/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,13 @@ namespace comic_book_gallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the Comic Books Controller";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from the comic books control");
         }
     }
 }
