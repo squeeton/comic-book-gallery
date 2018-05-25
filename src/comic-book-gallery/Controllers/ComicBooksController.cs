f8ce8bbc9ac245428d35fa0208230458097b6675
Matthew Standlee <mwstandlee@gmail.com>
2018-05-25 13:00:21 -0500

    added comic book detail view

--- src/comic-book-gallery/Controllers/ComicBooksController.cs
+++ src/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -10,11 +10,7 @@ namespace comic_book_gallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books control");
+            return View();
         }
     }
 }
