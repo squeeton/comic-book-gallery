e3a50ccfdf05e1c26fcacbd3e639d2a0db4bc79d
Matthew Standlee <mwstandlee@gmail.com>
2018-05-29 09:14:45 -0500

    Updated Comic books controller to use repository

--- src/comic-book-gallery/Models/ComicBook.cs
+++ src/comic-book-gallery/Models/ComicBook.cs
@@ -7,7 +7,7 @@ namespace comic_book_gallery.Models
 {
     public class ComicBook
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
